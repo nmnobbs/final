a4d7a941bd67f6dd44b588a921a7861ead1bfb7d
nmnobbs <77033963+nmnobbs@users.noreply.github.com>
2021-03-10 12:53:48 -0800

    final upload

--- app_server.R
+++ app_server.R
@@ -3,44 +3,66 @@
 library(shiny)
 library(tidyverse)
 
+# Read in Dataset---------------------------------------------------------------
+
+dataset <- read.csv("https://query.data.world/s/x7qrjzoufcjzx6agzg5d6omncpzup7",
+                    header = TRUE, stringsAsFactors = FALSE
+)
+
+names(dataset)[names(dataset) == "SOC1"] <-
+  "how much do you trust your neighborhood"
+names(dataset)[names(dataset) == "SOC2A"] <- "talk with neighbor in month"
+names(dataset)[names(dataset) == "SOC2B"] <-
+  "talk with neighbor in month before covid"
+names(dataset)[names(dataset) == "SOC3A"] <-
+  "how often did you stay in touch with family/friends"
+names(dataset)[names(dataset) == "SOC3B"] <-
+  "how often did you stay in touch with family/friend before covid"
+names(dataset)[names(dataset) == "SOC4A"] <-
+  "did you volunteer this month"
+names(dataset)[names(dataset) == "SOC4B"] <-
+  "how often did you volunteer per month before covid"
+names(dataset)[names(dataset) == "PHYS8"] <-
+  "how would you rate your health"
+names(dataset)[names(dataset) == "SOC5"] <-
+  "in the past 7 days have you felt anxious/depressed/lonely/hopeless"
+names(dataset)[names(dataset) == "GENDER"] <- "gender"
+names(dataset)[names(dataset) == "RACETH"] <- "race"
+names(dataset)[names(dataset) == "HHSIZE1"] <- "household size"
+
 ## Charts-----------------------------------------------------------------------
 
 # Scatterplot
 
-dataset2 <- dataset %>%  select(`how much do you trust your neighborhood`:
-                `how would you rate your health`, gender , race, RACE_R2 ,
-                EDUCATION, `household size`, P_GEO)
-chart3 <- dataset2 %>% 
-  filter(`how would you rate your health` !=  "(98) SKIPPED ON WEB") %>% 
-  filter(`household size` != "(99) REFUSED") %>% 
+dataset2 <- dataset %>% select(
+  `how much do you trust your neighborhood`:
+  `how would you rate your health`, gender, race, RACE_R2,
+  EDUCATION, `household size`, P_GEO
+)
+chart3 <- dataset2 %>%
+  filter(`how would you rate your health` != "(98) SKIPPED ON WEB") %>%
+  filter(`household size` != "(99) REFUSED") %>%
   filter(`household size` != "(98) SKIPPED ON WEB")
 
 Scatterplot <- ggplot(data = chart3) +
-  geom_count(mapping = aes(x= `how would you rate your health`, y =
-                             `household size`))
+  geom_count(mapping = aes(
+    x = `how would you rate your health`, y =
+      `household size`
+  ))
 
 # Code for Barchart
 
-data_items <- dataset3 %>% 
-  select(`1`:`4`) %>%
-  na.omit 
-
-Barchart <- data_items %>%  
-  gather(key = Questions, value = Responses) %>% 
-  mutate(Responses = factor(Responses)) %>% 
-  ggplot(aes(x = Questions)) + 
-  geom_bar(aes(fill = Responses), position = "fill") -> interactions
-interactions + scale_fill_brewer(palette = "Spectral")
-
-
-dataset3 <- dataset2 %>% 
-  select( `talk with neighbor in month` :
-            `how often did you stay in touch with family/friend before covid`,
-          gender) %>%
+dataset3 <- dataset2 %>%
+  select(
+    `talk with neighbor in month`:
+    `how often did you stay in touch with family/friend before covid`,
+    gender
+  ) %>%
   filter(gender != "(98) SKIPPED ON WEB") %>%
   filter(`talk with neighbor in month` != "(98) SKIPPED ON WEB")
 
-dataset3$`talk with neighbor in month` <- as.character(dataset3$`talk with neighbor in month`)
+dataset3$`talk with neighbor in month` <-
+  as.character(dataset3$`talk with neighbor in month`)
 dataset3[dataset3 == "(1) Basically every day"] <- "Every day"
 dataset3[dataset3 == "(2) A few times a week"] <- "few times a / W"
 dataset3[dataset3 == "(3) A few times a month"] <- "few times a / M"
@@ -55,11 +77,22 @@ dataset3$`talk with neighbor in month` <-
 
 names(dataset3)[names(dataset3) == "talk with neighbor in month"] <- "1"
 names(dataset3)[names(dataset3) ==
-                  "talk with neighbor in month before covid"] <- "2"
+  "talk with neighbor in month before covid"] <- "2"
 names(dataset3)[names(dataset3) ==
-                  "how often did you stay in touch with family/friends"] <- "3"
+  "how often did you stay in touch with family/friends"] <- "3"
 names(dataset3)[names(dataset3) ==
-      "how often did you stay in touch with family/friend before covid"] <- "4"
+  "how often did you stay in touch with family/friend before covid"] <- "4"
+
+data_items <- dataset3 %>%
+  select(`1`:`4`) %>%
+  na.omit()
+
+Barchart <- data_items %>%
+  gather(key = Questions, value = Responses) %>%
+  mutate(Responses = factor(Responses)) %>%
+  ggplot(aes(x = Questions)) +
+  geom_bar(aes(fill = Responses), position = "fill") -> interactions
+interactions + scale_fill_brewer(palette = "Spectral")
 
 # Code for Histogram
 
@@ -82,113 +115,118 @@ dataset_3$`how would you rate your health` <-
 names(dataset_3)[names(dataset_3) == "how would you rate your health"] <-
   "Mental Health Rating"
 
-Histogram <- ggplot(data = dataset_3, mapping = aes(x = `Mental Health Rating`,
-                                    fill = `Mental Health Rating`)) + 
+Histogram <- ggplot(data = dataset_3, mapping = aes(
+  x = `Mental Health Rating`,
+  fill = `Mental Health Rating`
+)) +
   geom_histogram(binwidth = 1, stat = "count") +
   labs(x = "Level of Mental Health", y = "Amount of Responses") +
-  facet_wrap(~gender, labeller = "label_both") 
+  facet_wrap(~gender, labeller = "label_both")
 
 # Define the server-------------------------------------------------------------
 
 server <- function(input, output) {
-
-output$Barchart <- renderPlot({
+  output$Barchart <- renderPlot({
     chart_data <- dataset3
     colnames(chart_data) <- c(
       "1", "2", "3", "4", "gender"
     )
-    
+
     bar <- ggplot(chart_data) +
-      geom_bar(aes(x = dataset3[[input$questions]]), fill = input$color_var
-      ) + facet_wrap(~gender, labeller = "label_both") +
+      geom_bar(aes(x = dataset3[[input$questions]]), fill = input$color_var) +
+      facet_wrap(~gender, labeller = "label_both") +
       labs(x = "Answer to Question Asked", y = "Amount of Responses")
-    
+
     bar
   })
 
-output$Histogram <- renderPlot({
-
-    third_c <- dataset_3 
+  output$Histogram <- renderPlot({
+    third_c <- dataset_3
     data.frame(gender = sample(c("Female", "Male"), 7494, replace = TRUE))
-    third_c$Female = ifelse(third_c$gender == "(2) Female", 1, 0)
-    third_c$Male = ifelse(third_c$gender == "(1) Male", 1, 0)
-    
+    third_c$Female <- ifelse(third_c$gender == "(2) Female", 1, 0)
+    third_c$Male <- ifelse(third_c$gender == "(1) Male", 1, 0)
+
     histogram <- third_c %>%
       filter(Female != "0") %>%
       filter(Male != "0") %>%
-       ggplot(data = third_c, mapping = aes(
-      x =
-        `Mental Health Rating`, fill = `Mental Health Rating`)) +
+      ggplot(data = third_c, mapping = aes(
+        x =
+          `Mental Health Rating`, fill = `Mental Health Rating`
+      )) +
       geom_histogram(binwidth = 1, stat = "count") +
       labs(x = "Level of Mental Health", y = "Amount of Responses") +
       facet_wrap(third_c[[input$gender_var]], labeller = "label_value") +
-      scale_fill_brewer(palette= input$colors_var2)
-    
+      scale_fill_brewer(palette = input$colors_var2)
+
     histogram
-    })
-  
-output$Scatterplot <- renderPlot({
-  
-  dataset2 <- dataset %>%  select(`how much do you trust your neighborhood`:
-                  `how would you rate your health`, gender , race, RACE_R2 ,
-                  EDUCATION, `household size`, P_GEO)
-  chart3 <- dataset2 %>% 
-    filter(`how would you rate your health` !=  "(98) SKIPPED ON WEB") %>%
-    filter(`how would you rate your health` != "(77) DON'T KNOW") %>%
-    filter(`household size` != "(99) REFUSED") %>% 
-    filter(`household size` != "(98) SKIPPED ON WEB") %>%
-    filter(`race` != "(98) SKIPPED ON WEB") %>%
-    filter(`race` != "(99) REFUSED") %>%
-    filter(`race` != "(88) Removed for disclosure risk") %>%
-    filter(gender != "(98) SKIPPED ON WEB")
-  
-  chart3$gender <-
-    as.character(chart3$gender)
-  chart3[chart3 == "(1) Male"] <- "Male"
-  chart3[chart3 == "(2) Female"] <- "Female"
-  chart3$gender <-
-    as.factor(chart3$gender)
-  
-  chart3$race <-
-    as.character(chart3$race)
-  chart3[chart3 == "(3) Hispanic"] <- "Hispanic"
-  chart3[chart3 == "(1) White, non-Hispanic"] <- "White, non-Hispanic"
-  chart3[chart3 == "(2) Black, non-Hispanic"] <- "Black, non-Hispanic"
-  chart3[chart3 == "(4) Other, non-Hispanic"] <- "Other, non-Hispanic"
-  chart3$race <-
-    as.factor(chart3$race)
-  
-  chart3$`how would you rate your health` <-
-    as.character(chart3$`how would you rate your health`)
-  chart3[chart3 == "(1) Excellent"] <- "Excellent"
-  chart3[chart3 == "(2) Very good"] <- "Very Good"
-  chart3[chart3 == "(3) Good"] <- "Good"
-  chart3[chart3 == "(4) Fair"] <- "Fair"
-  chart3[chart3 == "(5) Poor"] <- "Poor"
-  chart3$`how would you rate your health` <-
-    as.factor(chart3$`how would you rate your health`)
-  
-  chart3$`household size` <-
-    as.character(chart3$`household size`)
-  chart3[chart3 == "(1) One person, I live by myself"] <-
-    "1"
-  chart3[chart3 == "(2) Two persons"] <- "2"
-  chart3[chart3 == "(3) Three persons"] <- "3"
-  chart3[chart3 == "(4) Four persons"] <- "4"
-  chart3[chart3 == "(5) Five persons"] <- "5"
-  chart3[chart3 == "(6) Six or more persons"] <- "6+"
-  chart3$`household size` <-
-    as.factor(chart3$`household size`)
-  
-  Scatterplot <- ggplot(data = chart3) +
-    geom_count(mapping = aes(x= `how would you rate your health`,
-                             y = `household size`, color = ..n..)) +
-    scale_color_gradient(low = input$low_color, high = input$high_color) +
-    facet_wrap(input$category, labeller = "label_both") +
-    labs(x = "Rating of Mental Health", y =
-           "Household Size by Number of People")
-  
-  Scatterplot
   })
- 
-}  
+
+  output$Scatterplot <- renderPlot({
+    dataset2 <- dataset %>% select(
+      `how much do you trust your neighborhood`:
+      `how would you rate your health`, gender, race, RACE_R2,
+      EDUCATION, `household size`, P_GEO
+    )
+    chart3 <- dataset2 %>%
+      filter(`how would you rate your health` != "(98) SKIPPED ON WEB") %>%
+      filter(`how would you rate your health` != "(77) DON'T KNOW") %>%
+      filter(`household size` != "(99) REFUSED") %>%
+      filter(`household size` != "(98) SKIPPED ON WEB") %>%
+      filter(`race` != "(98) SKIPPED ON WEB") %>%
+      filter(`race` != "(99) REFUSED") %>%
+      filter(`race` != "(88) Removed for disclosure risk") %>%
+      filter(gender != "(98) SKIPPED ON WEB")
+
+    chart3$gender <-
+      as.character(chart3$gender)
+    chart3[chart3 == "(1) Male"] <- "Male"
+    chart3[chart3 == "(2) Female"] <- "Female"
+    chart3$gender <-
+      as.factor(chart3$gender)
+
+    chart3$race <-
+      as.character(chart3$race)
+    chart3[chart3 == "(3) Hispanic"] <- "Hispanic"
+    chart3[chart3 == "(1) White, non-Hispanic"] <- "White, non-Hispanic"
+    chart3[chart3 == "(2) Black, non-Hispanic"] <- "Black, non-Hispanic"
+    chart3[chart3 == "(4) Other, non-Hispanic"] <- "Other, non-Hispanic"
+    chart3$race <-
+      as.factor(chart3$race)
+
+    chart3$`how would you rate your health` <-
+      as.character(chart3$`how would you rate your health`)
+    chart3[chart3 == "(1) Excellent"] <- "Excellent"
+    chart3[chart3 == "(2) Very good"] <- "Very Good"
+    chart3[chart3 == "(3) Good"] <- "Good"
+    chart3[chart3 == "(4) Fair"] <- "Fair"
+    chart3[chart3 == "(5) Poor"] <- "Poor"
+    chart3$`how would you rate your health` <-
+      as.factor(chart3$`how would you rate your health`)
+
+    chart3$`household size` <-
+      as.character(chart3$`household size`)
+    chart3[chart3 == "(1) One person, I live by myself"] <-
+      "1"
+    chart3[chart3 == "(2) Two persons"] <- "2"
+    chart3[chart3 == "(3) Three persons"] <- "3"
+    chart3[chart3 == "(4) Four persons"] <- "4"
+    chart3[chart3 == "(5) Five persons"] <- "5"
+    chart3[chart3 == "(6) Six or more persons"] <- "6+"
+    chart3$`household size` <-
+      as.factor(chart3$`household size`)
+
+    Scatterplot <- ggplot(data = chart3) +
+      geom_count(mapping = aes(
+        x = `how would you rate your health`,
+        y = `household size`, color = ..n..
+      )) +
+      scale_color_gradient(low = input$low_color, high = input$high_color) +
+      facet_wrap(input$category, labeller = "label_both") +
+      labs(
+        x = "Rating of Mental Health", y =
+          "Household Size by Number of People"
+      )
+
+    Scatterplot
+  })
+}
--- app_ui.R
+++ app_ui.R
@@ -29,46 +29,58 @@ colors_var2 <- selectInput(
 questions_covid <- selectInput(
   inputId = "questions", label = "Choose Question",
   choices = c(
-    "1", "2", "3", "4")
+    "1", "2", "3", "4"
+  )
 )
 
 genders <- selectInput(
   inputId = "gender_var", label = "Choose Gender",
   choices = c(
-    "Male", "Female")
+    "Male", "Female"
+  )
 )
 
-choose_category <- radioButtons("category", "Choose Category",
-                             c("gender", "race"))
+choose_category <- radioButtons(
+  "category", "Choose Category",
+  c("gender", "race")
+)
 
 lowest_color <- selectInput(
   inputId = "low_color", label = "Choose Color for Lowest Value",
   choices = c(
-    "black", "blue", "brown", "purple")
+    "black", "blue", "brown", "purple"
+  )
 )
 
 highest_color <- selectInput(
   inputId = "high_color", label = "Choose Color for Highest Value",
   choices = c(
-    "red", "orange", "green", "pink")
+    "red", "orange", "green", "pink"
+  )
 )
 # Define structure of tabs------------------------------------------------------
 
 intro_page <- tabPanel(
   "Introduction",
-  h1("Welcome to our project"),
+  h1("Welcome to our project: Mental Health in the time of COVID-19"),
   fluidPage(
-      h1("Introduction"),
-p("For our interactive dataset research project our group decided to research mental health statistics
-due to COVID-19. Given the unprecedented times we are living in there has been an increase in mental health awareness.
-This dataset has provided us unique questions that allow us to see how people are reacting to the Covid pandemic during their daily life. 
-Questions that measure the amount of time people communicate with their family/friends, rating ones mental health from excellent to poor, and using 
-their gender as a means of comparison this data gives us an insight on how COVID-19 has impacted our mental health before the pandemic and during.
-Now more than ever are we more aware of taking care of one's mental health and we can use this data to better understand Covid-19's affect on individuals."),
-img(src = "https://media.giphy.com/media/FDZdbl2FjfoULzLH12/giphy.gif"),
-p(strong("By: Toby Theodros, Nicole Nobbay and Jimena Talamantes"))
-    )
-    )
+    h1("Introduction"),
+    p("For our interactive dataset research project our group decided to
+    research mental health statistics due to COVID-19. Given the unprecedented
+    times we are living in there has been an increase in mental health
+    awareness. This dataset has provided us unique questions that allow us to
+    see how people are reacting to the Covid pandemic during their daily life.
+    Questions that measure the amount of time people communicate with their
+    family/friends, rating ones mental health from excellent to poor, and using
+    their gender as a means of comparison this data gives us an insight on how
+    COVID-19 has impacted our mental health before the pandemic and during. Now
+      more than ever are we more aware of taking care of one's mental health
+      and we can use this data to better understand COVID-19's affect on
+      individuals."),
+    img(src = "https://media.giphy.com/media/FDZdbl2FjfoULzLH12/giphy.gif"),
+    p(strong("By: Toby Theodros, Nicole Nobbay and Jimena Talamantes"))
+  )
+)
 
 
 page_two <- tabPanel(
@@ -81,12 +93,18 @@ page_two <- tabPanel(
     mainPanel(
       plotOutput("Barchart"),
       p(strong("KEY")),
-      p("In this chart we are comparing people’s responses on how often 
-        they talked to their neighbors before and during COVID-19"),
-      p("Question 1 asked the question “How often did you talk to your neighbors?"),
-      p("Question 2 asks “How often did you talk to your neighbors during COVID-19?"),
-      p("Question 3 asks “How often did you stay in touch with your family and friends during quarantine?"),
-      p("Question 4 asks “How often did you stay in touch with your family and friends BEFORE COVID-19?"),
+      p("In this chart we are comparing people's responses on how often they
+        talked to their neighbors before and during COVID-19"),
+      p("Question 1 asked the question",
+        code("How often did you talk to your neighbors?")),
+      p("Question 2 asks",
+        code("How often did you talk to your neighbors during COVID-19?")),
+      p("Question 3 asks",
+        code("How often did you stay in touch with your family and friends
+             during quarantine?")),
+      p("Question 4 asks",
+        code("How often did you stay in touch with your family and friends
+             BEFORE COVID-19?")),
     )
   )
 )
@@ -100,12 +118,13 @@ page_three <- tabPanel(
     ),
     mainPanel(
       plotOutput("Histogram"),
-    p("When you select Male look at the chart where it says 1"),
-    p("When you select Female look at the chart where it says 1"),
-    p("Depending on which table you want to look at on the other side you 
-    will see the comparision versus the other gender")
+      p("When you select Male look at the chart where it says 1"),
+      p("When you select Female look at the chart where it says 1"),
+      p("Depending on which table you want to look at on the other side you will
+        see the comparision versus the other gender")
     )
-  ))
+  )
+)
 
 page_four <- tabPanel(
   "Scatterplot",
@@ -123,24 +142,37 @@ page_four <- tabPanel(
 
 sum_page <- tabPanel(
   "Summary Takeaways",
-   fluidPage(
+  fluidPage(
     h1("Summary"),
-    p("For our barchart we thought it was important to include this data because it demonstrates how more people reached out to their friends and family 
-when the pandemic started compared to when life was “normal”. 
-An interesting observation we made was that the number of people who talked to their family and friends “Basically every day” increased greatly once Covid began, and
-people who only talked to their family and friends “A few times a week” decreased. 
-We think there was a decrease because those people who used to be in the “A few times a week” category likely began communicating with friends and family more consistently."),
-p("In our histogram, we wanted to showcase the similarities between each gender and their personal ratings for their mental health. 
-This histogram is organized and clear in a manner that reflects a pattern of a high number of “Very Good” and “Good” ratings across both males and females. 
-The purpose of this chart is to break down the data set from one question to gather insight about how males and females have viewed their level of mental health.
-Some notable observations and insights from the chart include the evidence of how most males and females rated their level of mental health as “Very Good”."),
-p("In our scatterplot we wanted to answer the question how does the size of a household affect how people rate their mental health. 
-This chart is important because it shows how much having another person in your household affects the way you are feeling mentally.
-The scatterplot shows that the bigger the bubble is the more people that are in the category.
-The column that shows us the most clear evidence of covid’s impact on people who live by themselves is the people who feel “poor” column.
-As the number of people in a household increases the less people say that they feel “poor”.
-")
-)
+    p("For our barchart we thought it was important to include this data
+    because it demonstrates how more people reached out to their friends and
+    family when the pandemic started compared to when life was 'normal'. An
+    interesting observation we made was that the number of people who talked
+    to their family and friends", code("Basically every day"), "increased
+    greatly once Covid began, and people who only talked to their family and
+    friends", code("A few times a week"), "decreased. We think there was a
+      decrease because those people who used to be in the",
+      code("A few times a week"), "category likely began communicating with
+      friends and family more consistently."),
+    p("In our histogram, we wanted to showcase the similarities between each
+    gender and their personal ratings for their mental health. This histogram
+    is organized and clear in a manner that reflects a pattern of a high number
+    of", code("Very Good"), "and", code("Good"), "ratings across both males and
+    females. The purpose of this chart is to break down the data set from one
+    question to gather insight about how males and females have viewed their
+    level of mental health. Some notable observations and insights from the
+      chart include the evidence of how most males and females rated their
+      level of mental health as", code("Very Good")),
+    p("In our scatterplot we wanted to answer the question how does the size of
+    a household affect how people rate their mental health. This chart is
+    important because it shows how much having another person in your household
+    affects the way you are feeling mentally. The scatterplot shows that the
+    bigger the bubble is the more people that are in the category. The column
+    that shows us the most clear evidence of COVID's impact on people who live
+    by themselves is the people who feel", code("Poor"), "column. As the number
+      of people in a household increases the less people say that they feel",
+      code("Poor"), ".")
+  )
 )
 
 # Define the UI-----------------------------------------------------------------
